ee178742f9f3c3b29d4d1847c5fd2c2da828c16f
Ava Lawless <avalawless@Avas-MacBook-Pro.local>
2026-09-25 10:15:00 -0400

    My work on 04_apis

--- 04_apis/02_errors.py
+++ 04_apis/02_errors.py
@@ -4,14 +4,18 @@
 import httpx
 import json
 
-USER = "schaconxyz"
+USER = "schaconx"
 URL = "https://api.github.com/users/{user}/events/public"
 
-response = httpx.get(URL.format(user=USER))
-
-data = response.json()
+try:
+	response = httpx.get(URL.format(user=USER))
+	response.raise_for_status()
+	data = response.json()
 
 # print(json.dumps(data, indent=2))
 
-for item in data:
-	print(item["repo"]["name"], " - ", item["type"])
+	for item in data:
+		print(item["repo"]["name"], " - ", item["type"])
+
+except httpx.HTTPError as e:
+	print(e)
